c0ea5c02d706a6f7e36ca0d6fcb8c1d6d6f2f19f
unbanksytv <livethelife.tv@gmail.com>
2023-01-08 16:05:39 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,17 @@ import Head from 'next/head';
 const Home = () => {
   return (
     <div className="root">
-      <Head>
-        <title>GPT-33 | LTL</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
             <h1>LIVE THE LIFE</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Ask Me Anything</h2>
+            <h2>LFG ANON, ASK ME ANYTHING</h2>
           </div>
+        </div>
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box" />
         </div>
       </div>
     </div>
